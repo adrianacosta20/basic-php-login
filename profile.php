1e664e24fded9e1943eb0a4250de134a924ab40c
Adrian Acosta <aacosta@techlaunch>
2018-08-21 18:43:31 -0400

    update profile added

--- profile.php
+++ profile.php
@@ -15,5 +15,12 @@
     <li>Class: <?=$class?></li>
 </ul>
 
+<form action="profilesubmit.php" method="post">
+<input type="text" name="name" placeholder="Type your name"  /><br />
+<input type="email"  name="email" placeholder="Type your email"  /><br />
+<input type="text"  name="class" placeholder="Type your class name"  /><br />
+<input type="submit"  value = "update" />
+</form>
+
 <a href="home.php">Home</a><br/>
 <a href="logoutSubmit.php">Logout</a>
